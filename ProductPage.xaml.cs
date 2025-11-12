24fe86582f88b1851009f90e752dd2a8b02b1763
Apuuka2 <mainsteamakk27@gmail.com>
2025-11-12 18:33:31 +0500

    Добавьте файлы проекта.

---
+++ ProductPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿using System.Linq;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Zianberdin41размер
+{
+    /// <summary>
+    /// Логика взаимодействия для ProductPage.xaml
+    /// </summary>
+    public partial class ProductPage : Page
+    {
+        public ProductPage()
+        {
+            InitializeComponent();
+            var currentServices = Zianberdin41Entities.GetContext().Product.ToList();
+
+            ProductListView.ItemsSource = currentServices;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            ZianberdinClass.MainFrame.Navigate(new AddEditPage());
+        }
+    }
+}
